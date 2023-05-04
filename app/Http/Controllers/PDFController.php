a809f895cb92d90bc5c4f2ee8d0ef9432d55be01
Esteban Mella <63463792+Esteban-Mella@users.noreply.github.com>
2023-05-04 09:29:43 -0400

    operativo con bd un elemento

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -7,26 +7,33 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Barryvdh\DomPDF\Facade\Pdf;
 use App\historialentregas;
+use Psy\CodeCleaner\EmptyArrayDimFetchPass;
 
 class PDFController extends Controller
 {
     public function PDF(Request $request){
         $datosTabla = $request->input('datos');
         $informacion = $request->input('informacion');
-
+        $nroInventario = '';
+        $nro_activo_fijo = '';
+        $nro_serie = '';
+        $nombre_equipo = '';
+        $rutapdf='';
 
         $nombreArchivo ='pdf-'.$informacion[1].'-'.date("d M j G-i-s T Y").'.pdf';
-
         $pdf = PDF::loadView('pdf.PDFEntregaEquipos', ['datosTabla' => $datosTabla], ['informacion' => $informacion]);
 
-        $historialentregas = new historialentregas;
-        $historialentregas->asignado ='si';
-        $historialentregas->save();
+        foreach($datosTabla as $fila){
+            $nroInventario=$nroInventario.','.$fila[1];
+        }
+
 
 
         if ($informacion[1]==='Entrega') {
-
             Storage::disk('cargaEntrega')->put($nombreArchivo, $pdf->output());
+            $historialentregas = new historialentregas;
+            $historialentregas->nro_inventario = $nroInventario;
+            $historialentregas->save();
         } else {
             Storage::disk('cargaRetorno')->put($nombreArchivo, $pdf->output());
         }
@@ -34,5 +41,9 @@ class PDFController extends Controller
 
 
         return $pdf->stream('archivo.pdf');
+    }
+
+    public function guardarArchivo(){
+
     }
 }
